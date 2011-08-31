e57297673f96b7e411ccc2801cb89b1bfb0de7ce
Jérôme Petazzoni <jp@enix.org>
2011-08-31 01:00:19 -0700

    Add Database Credentials to settings.py
    Now, we need to edit ``settings.py`` to specify how to connect to our
    database. When you deploy your application, these parameters are stored
    in the DotCloud Environment File.
    This allows you to repeat the deployment of your application
    (e.g. for staging purposes) without having to manually copy-paste
    the parameters into your settings each time.
    
    If you don't want to use the Environment File, you can retrieve the
    same information with ``dotcloud info djangomongo.db``.
    
    The Environment File is a JSON file holding a lot of information about
    our stack. It contains (among other things) our database connection
    parameters. We will load this file, and use those parameters in Django's
    settings.
    
    See http://docs.dotcloud.com/guides/environment/ for more details about
    the Environment File.
    
    ..
    
    .. note::
    
       We decided to use the ``admin`` database here. This was made
       to simplify the configuration process. While you can actually use
       any database name you like (MongoDB will create it automatically),
       MongoDB admin accounts have to authenticate against the ``admin``
       database, as explained in `MongoDB Security and Authentication docs
       <http://www.mongodb.org/display/DOCS/Security+and+Authentication>`_.
       If you want to use another database, you will have to create a
       separate user manually, or add some extra commands to the
       ``postinstall`` script shown in next sections.
    
    .. note::
    
       You might wonder why we put the MongoDB connection URL in the
       ``HOST`` parameter! Couln't we just put the hostname, and then
       also set ``USER``, ``PASSWORD``, and ``PORT``? Well, we could.
       However, when we will want to switch to replica sets, we will
       have to specify multiple host/port combinations. And one convenient
       way to do that is to use the `Standard Connection String Format
       <http://www.mongodb.org/display/DOCS/Connections>`_.
    
       Quite conveniently, ``django_mongodb_engine`` will just pass
       the database parameters to ``pymongo``'s ``Connection``
       constructor. By using the ``mongodb://`` URL as our ``HOST``
       field, we're actually handing it to ``pymongo``, which will
       do The Right Thing.

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -1,5 +1,9 @@
 # Django settings for hellodjango project.
 
+import json
+with open('/home/dotcloud/environment.json') as f:
+  env = json.load(f)
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -11,12 +15,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django_mongodb_engine',
+        'NAME': 'admin',
+        'HOST': env['DOTCLOUD_DB_MONGODB_URL'],
+        'SUPPORTS_TRANSACTIONS': False,
     }
 }
 
