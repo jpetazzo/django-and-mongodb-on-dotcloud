c39455eef8eb9e4a95620a8d6db2ae3e6c9d8e16
Jérôme Petazzoni <jp@enix.org>
2011-09-07 14:04:16 -0700

    Disable "sites" and Enable "djangotoolbox"
    By default, the application ``django.contrib.sites`` won't behave
    well with the Django MongoDB engine. Under the hood, it boils down
    to differences in primary keys, which are strings with MongoDB,
    and integers elsewhere. It would of course be more elegant to fix
    ``sites`` in the first place, but for the sake of simplicity, we
    will just disable it since we don't need it for simple apps.
    
    Also, we need ``djangotoolbox`` to make user editing in the
    admin site work correctly. Long story short, ``djangotoolbox``
    allows us to do some JOINs on non-relational databases.

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -114,13 +114,14 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
+    #'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'djangotoolbox',
 )
 
 # A sample logging configuration. The only tangible logging
